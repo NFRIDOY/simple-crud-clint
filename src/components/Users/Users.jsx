dad2e592d916bad2374bbd9e6670c5072e537a67
NF RIDOY <nfridoy@gmail.com>
2023-10-14 16:07:56 +0600

    Feat: handleDelete

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,6 +5,29 @@ export default function Users() {
     const users = useLoaderData();
 
     console.log(users)
+
+    const handleDelete = (_id) => {
+        console.log('deleting', _id)
+        fetch(`http://localhost:5000/users/${_id}`,{
+            method: 'DELETE',
+            // headers: {
+            //     'content-type': 'application/json',
+            // },
+            // body: JSON.stringify()
+
+        })
+        .then(res => res.json())
+        .then(data => {
+            
+            console.log(data)
+            if(data.deletedCount>0) {
+                alert('deleted', _id, " ", data.deletedCount)
+            }
+            else {
+                console.log("Failed to delete", _id)
+            }
+        })        
+    }
     return (
         <div>
             <p>
@@ -14,7 +37,7 @@ export default function Users() {
                 {
                     users.map((user) => (
                         <p key={user._id}>
-                            {user.name}
+                            {user.name} : {user.email} : <button className='p-px border-2' onClick={() => handleDelete(user._id)}>X</button>
                         </p>
                     ))}
             </div>
