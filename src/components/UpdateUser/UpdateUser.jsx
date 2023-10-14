de1a33e9cfac7cab7b7bb8660d869db77ff1499b
NF RIDOY <nfridoy@gmail.com>
2023-10-14 22:03:27 +0600

    Feat: handleUpdate res functional

--- src/components/UpdateUser/UpdateUser.jsx
+++ src/components/UpdateUser/UpdateUser.jsx
@@ -11,6 +11,26 @@ export default function UpdateUser() {
     console.log(loadedUser)
     console.log(loadedUser.name)
 
+    const handleUpdate = (e) => {
+        e.preventDefault();
+        const form = e.target
+        const name = form.name.value
+        const email = form.email.value
+
+        const updatedUser = {name, email}
+        console.log(updatedUser)
+
+        fetch(`http://localhost:5000/user/${loadedUser._id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(updatedUser)
+        })
+        .then(res => res.json())
+        .then(data=> console.log(data))
+    }
+
     return (
         <div>
             {/* <h1>User Id {loadedUser?._id}</h1>
@@ -19,12 +39,12 @@ export default function UpdateUser() {
             <h1 className="my-10 text-3xl">Update Your Info</h1>
 
             <div className="w-1/2 mx-auto">
-                <form className="form-control">
-                    <input className="input input-bordered" type="text" name="name" placeholder="Name" defaultValue={name} />
+                <form className="form-control" onSubmit={handleUpdate}>
+                    <input className="input input-bordered" type="text" name="name" placeholder="Name" defaultValue={loadedUser?.name} />
                     <br />
-                    <input className="input input-bordered" type="text" name="email" placeholder="email" defaultValue={email} />
+                    <input className="input input-bordered" type="text" name="email" placeholder="email" defaultValue={loadedUser?.email} />
                     <br />
-                    <button className="btn btn-success">Update</button>
+                    <button className="btn btn-success" >Update</button>
                 </form>
             </div>
         </div>
